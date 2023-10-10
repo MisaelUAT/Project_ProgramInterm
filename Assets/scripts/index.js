ce396697c0d87f50a5ddb9c16edb867c3e75b92e
Misael Alejandro Hernandez Mendoza <143233780+MisaelUAT@users.noreply.github.com>
2023-10-10 15:48:16 +0000

    Estructuracion incompleta del form

--- Assets/scripts/index.js
+++ Assets/scripts/index.js
@@ -1,18 +1,1 @@
-const image= document.querySelectorAll('.control-img-socialmedia');
-
-
-
-
-//Functions
-
-// <--Footer-->
-image.forEach((socialmedia) => {
-    socialmedia.addEventListener('mouseenter',()=>{
-        socialmedia.classList.add('floating_img')
-
-    })
-    socialmedia.addEventListener('mouseleave',()=>{
-        socialmedia.classList.remove('floating_img')
-    })
-
-});
+console.log('Hello World!');
